b44a2032c2c4c319890ebc4c797018418770cd6d
myronova-git <myronova@int.pl>
2024-10-24 12:06:53 +0200

    add display_info

--- Person.py
+++ Person.py
@@ -5,3 +5,10 @@ class Person:
         self.__lastname = lastname
         self.__age = age
         self.__email = email
+
+    def display_info(self):
+        print(f"ID: {self.__id}")
+        print(f"Имя: {self.__firstname}")
+        print(f"Фамилия: {self.__lastname}")
+        print(f"Возраст: {self.__age}")
+        print(f"Электронная почта: {self.__email}")
